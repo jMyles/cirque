9af3782db6ed81e540279414d72deb64fe34058a
KPrasch <kieranprasch@gmail.com>
2014-08-20 17:06:33 -0400

    Initial front end commit - angular ui bootstrap

--- REQUIREMENTS.rst
+++ REQUIREMENTS.rst
@@ -1,4 +1,2 @@
-
 django
 hendrix
-
---
+++ app/js/tabs.js
@@ -0,0 +1,31 @@
+angular.module('cirque', ['ui.bootstrap']);
+var Tabs = function ($scope) {
+  $scope.tabs = [
+    { title:'Dynamic Title 1', content:'Dynamic content 1' },
+    { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+  ];
+
+  $scope.alertMe = function() {
+    setTimeout(function() {
+      alert('You\'ve selected the alert tab!');
+    });
+  };
+};
+
+
+
+function Alert($scope) {
+  $scope.alerts = [
+    { type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
+    { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
+  ];
+
+  $scope.addAlert = function() {
+    $scope.alerts.push({msg: 'Another alert!'});
+  };
+
+  $scope.closeAlert = function(index) {
+    $scope.alerts.splice(index, 1);
+  };
+
+}
